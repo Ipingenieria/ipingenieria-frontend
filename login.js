90f7447f788a17907d67d82b45831c3d2b124868
Ipingenieria <ipingenieriayautomatismo@gmail.com>
2025-04-20 13:36:24 -0500

    Add files via upload

--- login.js
+++ login.js
@@ -1,25 +1,12 @@
-// Esta es una estructura de ejemplo para el login. En la versión real, usarás una librería Supabase.
+// Protección de ruta
+const usuario = JSON.parse(localStorage.getItem("usuario"));
+if (!usuario) {
+  localStorage.setItem("redirigir_a", window.location.pathname.replace('/', ''));
+  window.location.href = "login.html";
+}
 
-document.getElementById('loginForm').addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const mensaje = document.getElementById('mensaje');
-
-  // Simulación: solo acepta estos usuarios
-  const users = {
-    "admin@ipingenieria.com": "Admin2025*",
-    "secretaria@ipingenieria.com": "Secret2025*",
-    "tecnico@ipingenieria.com": "Tecnico2025*"
-  };
-
-  if (users[email] && users[email] === password) {
-    localStorage.setItem("user", JSON.stringify({ email }));
-    mensaje.textContent = "Bienvenido. Redirigiendo...";
-    setTimeout(() => {
-      window.location.href = "index.html";
-    }, 1000);
-  } else {
-    mensaje.textContent = "Usuario o contraseña incorrecta.";
-  }
-});
+// Función de cierre
+function cerrarSesion() {
+  localStorage.removeItem("usuario");
+  window.location.href = "login.html";
+}
